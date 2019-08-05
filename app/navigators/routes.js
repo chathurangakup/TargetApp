c01b0b70e280c0060d0fa3e13a8bced134513f06
Yasith <yasith@easybooking.lk>
2019-08-05 09:15:34 +0530

    add routes

--- app/navigators/routes.js
+++ app/navigators/routes.js
@@ -8,10 +8,101 @@ import {
 
 import HomeScreen from '../screens/HomeScreen';
 
+import Splash from '../screens/Splash';
+import Login from '../screens/Login';
+import SunScreen from '../screens/SunScreen';
+import MonScreen from '../screens/MonScreen';
+import WheScreen from '../screens/WheScreen';
+import TueScreen from '../screens/TueScreen';
+import ThrusScreen from '../screens/ThrusScreen';
+import FriScreeen from '../screens/FriScreen';
+import SatScreen from '../screens/SatScreen';
+
+
+const SignedOut = createStackNavigator({
+    Splash: {
+        screen: Splash,
+        navigationOptions:{
+            header:()=> null,
+        }
+    },
+    Login: {
+        screen: Login,
+        navigationOptions:{
+            header:()=> null,
+        }
+    },
+  
+    SunScreen:{
+        screen: SunScreen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+
+    MonScreen:{
+        screen: MonScreen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+
+    TueScreen:{
+        screen: TueScreen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+    WheScreen:{
+        screen: WheScreen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+
+    ThrusScreen:{
+        screen: ThrusScreen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+
+    FriScreeen:{
+        screen: FriScreeen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+
+    SatScreen:{
+        screen: SatScreen,
+        navigationOptions: {
+            header:null,
+            }
+    },
+    
+   
+},{
+
+    navigationOptions: {
+        headerStyle: {
+            elevation: 0,
+            shadowOpacity: 0
+            }
+    }
+}
+
+);
+
+
+
+
+
 const DrawerNavigator=createDrawerNavigator({
     Home:{
         screen:HomeScreen,
     }
 });
 
-export default createAppContainer(DrawerNavigator)
+const App=createAppContainer(DrawerNavigator)
+export default App;
